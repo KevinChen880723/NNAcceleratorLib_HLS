46f5048856becf24b71e3581e91edd0dac93c469
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-01-16 21:55:16 +0800

    conv1 version 1 111/1/16 21:52

--- Layers/conv1.cpp
+++ Layers/conv1.cpp
@@ -23,23 +23,22 @@
 void ReadFromMem(
         unsigned short            width,
         unsigned short            height,
-        datatype                  *coeffs,
+        datatype                  *weights,
+        hls::stream<datatype>     &input_stream,
         hls::stream<datatype>     &coeff_stream,
-        const datatype            src,
         hls::stream<datatype>     &pixel_stream )
 {
     assert(height <= MAX_IMAGE_HEIGHT);
 
-    unsigned num_coefs = FILTER_V_SIZE*FILTER_H_SIZE;
-    unsigned num_coefs_padded = (((num_coefs-1)/64)+1)*64; // Make sure number of reads of multiple of 64, enables auto-widening
-    read_coefs: for (int i=0; i<num_coefs_padded; i++) {
-        datatype coef = coeffs[i];
-        if (i<num_coefs) coeff_stream.write( coef );
+    unsigned short num_coefs = FILTER_V_SIZE*FILTER_H_SIZE;
+    read_coefs: for (int i=0; i<num_coefs; i++) {
+        datatype coef = weights[i];
+        coeff_stream.write( coef );
     }
 
     read_image: for (int n = 0; n < height*width; n++) {
-        datatype pix = src[n];
-        if (x<width) pixel_stream.write( pix );
+        datatype pix = input_stream.read();
+        pixel_stream.write( pix );
     }
 }
 
@@ -79,7 +78,7 @@ void Window2D(
     unsigned col_ptr = 0;
     // Initializing time to fill the data that raquired by a window into the line buffer
     // In the official code, because of the zero padding this is the time to get half the data in the first window
-    unsigned ramp_up = (do_padding == true) ? width*((FILTER_V_SIZE-1)/2)+(FILTER_H_SIZE-1)/2: width*(FILTER_V_SIZE-1)+(FILTER_H_SIZE-1);
+    unsigned ramp_up = (do_padding == 1) ? width*((FILTER_V_SIZE-1)/2)+(FILTER_H_SIZE-1)/2: width*(FILTER_V_SIZE-1)+(FILTER_H_SIZE-1);
     unsigned num_pixels = width*height;
     unsigned num_iterations = num_pixels + ramp_up;
 
@@ -126,11 +125,9 @@ void Window2D(
 void Filter2D(
         unsigned short              width,
         unsigned short              height,
-        float                       factor,
-        short                       bias,
-        hls::stream<datatype>      &coeff_stream,
-        hls::stream<window>        &window_stream,
-		hls::stream<datatype>      &pixel_stream,
+        hls::stream<datatype>       &coeff_stream,
+        hls::stream<window>         &window_stream,
+		hls::stream<datatype>       &output_stream,
         ap_int<1>                   do_padding)
 {
     assert(width  <= MAX_IMAGE_WIDTH);
@@ -149,6 +146,7 @@ void Filter2D(
     }
 
     // If we don't want to pad the image, the output size will be shinked
+    // I only implemented stide=1, so the size after convolution will be (OriginalSize - WindowSize> + 1)
     height = (do_padding == true)? height: height-FILTER_V_SIZE+1;
     width = (do_padding == true)? width: width-FILTER_H_SIZE+1;
 
@@ -180,13 +178,8 @@ void Filter2D(
                     sum += pixel*(datatype)coeffs[row][col];
                 }
             } 
-
-            datatype outpix = sum+bias;
-            // // Normalize result
-            // unsigned datatype outpix = MIN(MAX((int(factor * sum)+bias), 0), 255);
-
             // Write the output pixel
-            pixel_stream.write(outpix);
+            output_stream.write(sum);
         }
     }
 }
@@ -199,46 +192,87 @@ extern "C" {
  */
 void Filter2DKernel(
         datatype                 *Wconv,
-        datatype                 Bconv,
-        unsigned short           width,
-        unsigned short           height,
-        hls::stream<datatype>    input,
-        hls::stream<datatype>    output)
+        unsigned short           width_input,
+        unsigned short           height_input,
+        hls::stream<datatype>    &input_stream,
+        hls::stream<datatype>    &output_stream)
     {
 #pragma HLS DATAFLOW
 
 	// Stream of pixels from kernel input to filter, and from filter to output
-    hls::stream<datatype,2>    coefs_stream;
+    hls::stream<datatype,2>      coefs_stream;
     hls::stream<datatype,2>      pixel_stream;
-    hls::stream<window,3>  window_stream; // Set FIFO depth to 0 to minimize resources
-    hls::stream<datatype,64>     output_stream;
+    hls::stream<window,3>        window_stream; // Set FIFO depth to 0 to minimize resources
+    // hls::stream<datatype,64>     output_stream;
 
 	// Read image data from global memory over AXI4 MM, and stream pixels out
-    ReadFromMem(width, height, Wconv, coefs_stream, src, pixel_stream);
+    ReadFromMem(width_input, height_input, Wconv, coefs_stream, input_stream, pixel_stream);
 
     // Read incoming pixels and form valid HxV windows
-    Window2D(width, height, pixel_stream, window_stream, 0);
+    Window2D(width_input, height_input, pixel_stream, window_stream, 0);
 
 	// Process incoming stream of pixels, and stream pixels out
-	Filter2D(width, height, bias, coefs_stream, window_stream, output_stream, 0);
+	Filter2D(width_input, height_input, coefs_stream, window_stream, output_stream, 0);
 
-	// Write incoming stream of pixels and write them to global memory over AXI4 MM
-	WriteToMem(width, height, output_stream, dst);
+	// // Write incoming stream of pixels and write them to global memory over AXI4 MM
+	// WriteToMem(width_input, height_input, output_stream, dst);
 
     }
 
+}
+
+void summation(
+        datatype                 bias,
+        unsigned short           width,
+        unsigned short           height,
+        unsigned short           num_channel,
+        hls::stream<datatype>    &ChannelOutput_stream,
+        hls::stream<datatype>    &OverallOutput_stream)
+{
+    datatype outputFeature[height][width];
+    for(int c = 0; c < num_channel; c++){
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                if (c == 0) outputFeature[y][x] = ChannelOutput_stream.read();
+                else{
+                    outputFeature[y][x] += ChannelOutput_stream.read();
+                    if (c == num_channel-1){
+                        OverallOutput_stream.write(outputFeature[y][x] + bias);
+                    }
+                }
+            }
+        }
+    }
 }
 
 void conv1(
-        datatype                 Wconv[layer2CnannelNum][FILTER_V_SIZE*FILTER_H_SIZE],
+        datatype                 Wconv[layer2CnannelNum][layer1CnannelNum][FILTER_V_SIZE*FILTER_H_SIZE],
         datatype                 Bconv[layer2CnannelNum],
-        unsigned short           width,
-        unsigned short           height,
-        hls::stream<datatype>    input,
-        hls::stream<datatype>    output)
+        unsigned short           width_input,
+        unsigned short           height_input,
+        hls::stream<datatype>    &input_stream,
+        hls::stream<datatype>    &OverallOutput_stream)
 {
-    ap_int<16> channel_o = 0;
-    for (channel_o = 0; channel_o < layer2CnannelNum; channel_o++){
-        Filter2DKernel()
+    hls::stream<datatype> ChannelOutput_stream;
+    unsigned short width_output   = width_input - FILTER_H_SIZE + 1;
+    unsigned short height_output  = height_input - FILTER_V_SIZE + 1;
+    unsigned short channel_input  = layer1CnannelNum;
+    unsigned short channel_output = layer2CnannelNum;
+    datatype outputFeature[height][width];
+    
+    // Execute convolution <layer2CnannelNum> times to get the output with <layer2CnannelNum> channels
+    for(int channel_num_o = 0; channel_num_o < channel_output; channel_num_o++)){
+        // Execute convolution for a kernel
+        for (int channel_num_i = 0; channel_num_i < channel_input; channel_num_i++){
+            // Do convolution on every channels, then send the output stream to summation module
+            Filter2DKernel(Wconv[channel_num_o][channel_num_i], width_input, height_input, input_stream, ChannelOutput_stream);
+        }
+        // Summation module sum the value in the same coordinate up then add by the bias
+        summation(Bconv[channel_num_o], width_input, height_input, channel_input, ChannelOutput_stream, OverallOutput_stream);
     }
 }
+
+/*
+ * 我不太確定最後一個Module中的summation()要放在回圈內還是外，放在裡面我怕會有三個一樣的硬體 (我要三次Filter2DKernel()都對應到同一個summation())，放外面不知道會不會等迴圈內容結束才執行?
+ * 感覺放裡面應該不會有三個一樣的硬體，因為我沒有Unroll。如果它變成三個硬體同步執行，我就不會知道他執行的順序是怎麼樣，summation()裡面預設一層一層跑的順序就不一定對了。
+ */
