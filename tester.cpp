a355e79673e32f8f5da94390752d10227e7c35ac
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-01-17 16:34:31 +0800

    read testing image successfully

--- tester.cpp
+++ tester.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "./MNIST.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
 	bool pass = 1;
-	fstream fstream("./hlsTest_float_folder/input.txt");
-	myDatatype data;
-	fstream >> data;
+
+	fstream fs;
+	string data;
+	myDatatype data_myDatatype;
+	fs.open("C:/Users/user/Desktop/Embbed_Application/MNIST_Inference_Device/hlsTest_float_folder/input.txt");
+	for (int i = 0; i < 784; i++){
+		getline(fs, data);
+		data_myDatatype = stof(data);
+		cout << data_myDatatype << endl;
+	}
+
 	if (pass == 0) {
 		cout << "test failed" << endl;
 		return 1;
